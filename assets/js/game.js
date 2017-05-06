b98520c3a5bda191bbf7fc2247c754db989c83dd
Rich Cross <richwesley@comcast.net>
2017-05-05 19:11:00 -0700

    Update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,23 @@
 // JavaScript Document
-var aBatch = 0;
-var bBatch = 0;
-var cBatch = 0; 
-var dBatch = 0;
+//initialize all global variables
+var aBatch 		= 0;
+var bBatch 		= 0;
+var cBatch 		= 0; 
+var dBatch 		= 0;
 var winCount    = 0;
 var lossCount   = 0;
 var randomVar   = 0;
 var playCounter = 0; 
 
 //$(document).ready(function() {
-	function openWin() {
-    window.open("info.html");
+function showInst() {
+    $('#info').text("Walt and Jesse have commissioned you to sell off some excess inventory.  Jesse has come up with a new way to unload the goods.  Rather than packaging it in predetermined quantities (ie a quater bag or eight ball), Jessie has decided to 'take orders' and  let the customer buy whatever quantity they want.  When you click the 'Get Order' button you will need to package it by choosing the correct amount of each crystal variety so that it weighs exactly the order quanity.  But, be carefull if you go over you will anger Walt and lose the order.  Good luck.");
+}
+
+function newGame () {
+	winCount   = 0;
+	lossCount  = 0;
+	startGame();
 }
 
 function startGame() {
@@ -21,7 +28,7 @@ function startGame() {
 	
 	randomVar = Math.floor(Math.random()*51)+9;
 	$('#orderWt').html(randomVar);
-	console.log(playCounter + " " + randomVar);
+	console.log("init Rndvar " + playCounter + " " + randomVar);
 
 	// set the random value for each crystal
 		aBatch = Math.floor(Math.random() *9)+4;
@@ -29,35 +36,40 @@ function startGame() {
 		cBatch = Math.floor(Math.random() *9)+4;
 		dBatch = Math.floor(Math.random() *9)+4;
 
-	console.log(aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
+	console.log("init crystals " + aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
 	
 	// add value of slected crystal to the Playcounter and check game status 
 	$('#c1').on('click',function () {
 		playCounter += aBatch;
 		$('#scaleWt').text(playCounter);
+		console.log("c1 " + aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);
 		checkStatus();
 		});
 	
 	$('#c2').on('click', function () {
 		playCounter += bBatch;
 		$('#scaleWt').text(playCounter);
+		console.log("c2 " + aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);
 		checkStatus();
 		});
 
 	$('#c3').on('click', function () {
 		playCounter += cBatch;
 		$('#scaleWt').text(playCounter);
+		console.log("c3 " + aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);
 		checkStatus();
 		});
 	
 	$('#c4').on('click', function () {
 		playCounter += dBatch;
 		$('#scaleWt').text(playCounter);
+		console.log("c4 " + aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);
 		checkStatus();
 		});	
-	 
-	
-		
 }
 // compare playCounter to randomVar and execute apprpriate action
 function checkStatus (){
@@ -66,16 +78,20 @@ function checkStatus (){
 		alert("you win");
 		winCount ++;
 		$('#wins').text(winCount);
+		console.log("if " + aBatch + " " +bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);
 		startGame();
 	}
 	else if (playCounter > randomVar) {
 		alert("You Lose");
 		lossCount ++;
 		$('#losses').text(lossCount);
+		console.log("else if" + aBatch + " " + bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);
 		startGame();
 	}
 	else {
-		console.log(playCounter + " " + randomVar);
-		return;
+		console.log(" else " + aBatch + " " + bBatch + " " + cBatch + " " + dBatch);
+		console.log(playCounter + " " + randomVar);	
 	}
 }
